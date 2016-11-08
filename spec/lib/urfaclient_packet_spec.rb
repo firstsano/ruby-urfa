7f3ca7307c16b413855409b23c8a345e56ae70bb
Koltunov Alexander <firstsano@gmail.com>
2016-11-08 14:40:05 +0300

    Add test for bin2long

--- spec/lib/urfaclient_packet_spec.rb
+++ spec/lib/urfaclient_packet_spec.rb
@@ -13,6 +13,10 @@ describe UrfaclientPacket do
       double: {
         string: "test string",
         value: 2.3556924101843323e+251
+      },
+      long: {
+        string: "test string",
+        value: 8387236823645254770
       }
     }
   end
@@ -25,6 +29,10 @@ describe UrfaclientPacket do
     it 'can unpack to double/float' do
       float = packet.bin2double @urfa_binary[:double][:string]
       expect(float).to eq(@urfa_binary[:double][:value])
+    end
+    it 'can unpack to long' do
+      long = packet.bin2long @urfa_binary[:long][:string]
+      expect(long).to eq(@urfa_binary[:long][:value])
     end
   end
 end
