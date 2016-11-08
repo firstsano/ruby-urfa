c51529a134b7fa69dc801d4f5739265f28c0f535
Koltunov Alexander <firstsano@gmail.com>
2016-11-08 18:30:49 +0300

    Implement first version of bin2long

--- lib/urfaclient_packet.rb
+++ lib/urfaclient_packet.rb
@@ -5,4 +5,28 @@ class UrfaclientPacket
   def bin2double(string)
     string.reverse.unpack("d*").first
   end
+
+  def bin2long(string)
+    hi, lo = string.unpack("N2")
+    cut_32bit_integer(lo) if value_greater_than_32bit_integer?(lo)
+    if value_greater_than_32bit_integer?(hi)
+      hi &= 0xffffffff
+      hi ^= 0xffffffff
+      lo ^= 0xffffffff
+      lo -= 1;
+      0 - hi * 4_294_967_296 - lo
+    else
+      hi * 4_294_967_296 + lo
+    end
+  end
+
+  private
+
+  def value_greater_than_32bit_integer?(value)
+    (value & 0x80000000) != 0
+  end
+
+  def cut_32bit_integer(value)
+    value & 0xffffffff
+  end
 end
