6617c27a14c38ff27f4caca3f2d790f0cbe194e6
Koltunov Alexander <firstsano@gmail.com>
2016-11-08 13:29:47 +0300

    Add bin2double methods

--- lib/urfaclient_packet.rb
+++ lib/urfaclient_packet.rb
@@ -2,4 +2,7 @@ class UrfaclientPacket
   def bin2int(string)
     string.unpack("N*").first
   end
+  def bin2double(string)
+    string.reverse.unpack("d*").first
+  end
 end
--- spec/lib/urfaclient_packet_spec.rb
+++ spec/lib/urfaclient_packet_spec.rb
@@ -5,15 +5,26 @@ require "rspec/its"
 describe UrfaclientPacket do
   subject(:packet) { UrfaclientPacket.new }
   before(:all) do
-    @connection_transformations = {
+    @urfa_binary = {
       int: {
         string: "test",
         value: 1952805748
+      },
+      double: {
+        string: "test string",
+        value: 2.3556924101843323e+251
       }
     }
   end
 
-  it 'should unpack binary string to integer' do
-    expect(packet.bin2int(@connection_transformations[:int][:string])).to eq(@connection_transformations[:int][:value])
+  context 'unpacking binary string' do
+    it 'can unpack to integer' do
+      integer = packet.bin2int @urfa_binary[:int][:string]
+      expect(integer).to eq(@urfa_binary[:int][:value])
+    end
+    it 'can unpack to double/float' do
+      float = packet.bin2double @urfa_binary[:double][:string]
+      expect(float).to eq(@urfa_binary[:double][:value])
+    end
   end
 end
