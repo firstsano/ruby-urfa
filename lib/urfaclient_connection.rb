80bc029b9d3901073e99346e2bd4a29b311a907f
Koltunov Alexander <firstsano@gmail.com>
2016-11-14 15:24:57 +0300

    Implement constructor test and initialize method

--- lib/urfaclient_connection.rb
+++ lib/urfaclient_connection.rb
@@ -1,3 +1,27 @@
 class UrfaclientConnection
   attr_accessor :error
+
+  def initialize(address, port, login, pass, ssl = true, admin = false)
+    @admin = admin
+    return connection_error unless open(address, port)
+    return login_error unless login(login, pass, true)
+  end
+
+  def open(*args)
+  end
+
+  def login(*args)
+  end
+
+  private
+
+  def connection_error
+    @error = 'connect error'
+    false
+  end
+
+  def login_error
+    @error = 'login error'
+    false
+  end
 end
--- spec/lib/urfaclient_connection_spec.rb
+++ spec/lib/urfaclient_connection_spec.rb
@@ -2,9 +2,33 @@ require "urfaclient_connection"
 require "rspec/its"
 
 describe UrfaclientConnection do
-  subject(:connection) { UrfaclientConnection.new }
+  let(:auth) do
+    OpenStruct.new({ address: "127.0.0.1", port: 80, login: "test", pass: "test" })
+  end
 
-  it 'should should respond to certain messages' do
-    expect(connection).to respond_to(:error)
+  context "initialize behavior" do
+    it 'should not allow to call initialize without arguments' do
+      expect{ UrfaclientConnection.new }.to raise_error(ArgumentError)
+    end
+
+    it 'should set error and return false on unsuccessful opening' do
+      expect_any_instance_of(UrfaclientConnection).to receive(:open).and_return(false)
+      expect_any_instance_of(UrfaclientConnection).to receive(:connection_error)
+      UrfaclientConnection.new(auth.address, auth.port, auth.login, auth.pass)
+    end
+
+    it 'should set error on unsuccessful logging' do
+      expect_any_instance_of(UrfaclientConnection).to receive(:open).and_return(true)
+      expect_any_instance_of(UrfaclientConnection).to receive(:login).and_return(false)
+      expect_any_instance_of(UrfaclientConnection).to receive(:login_error)
+      UrfaclientConnection.new(auth.address, auth.port, auth.login, auth.pass)
+    end
+
+    it 'should successfully create object whe no errors' do
+      expect_any_instance_of(UrfaclientConnection).to receive(:open).and_return(true)
+      expect_any_instance_of(UrfaclientConnection).to receive(:login).and_return(true)
+      connection = UrfaclientConnection.new(auth.address, auth.port, auth.login, auth.pass)
+      expect(connection).to be_an_instance_of(UrfaclientConnection)
+    end
   end
 end
