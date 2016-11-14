f7de45c1fd6d992ec35fe0432f9458b11668a10a
Koltunov Alexander <firstsano@gmail.com>
2016-11-14 14:13:27 +0300

    Add test 'respond to messages'

--- spec/lib/urfaclient_connection_spec.rb
+++ spec/lib/urfaclient_connection_spec.rb
@@ -1,2 +1,10 @@
 require "urfaclient_connection"
 require "rspec/its"
+
+describe UrfaclientConnection do
+  subject(:connection) { UrfaclientConnection.new }
+
+  it 'should should respond to certain messages' do
+    expect(connection).to respond_to(:error)
+  end
+end
