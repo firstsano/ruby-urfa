3905af799034274a5a8a3dfe861f35dcd43b2bc6
Koltunov Alexander <firstsano@gmail.com>
2016-11-18 16:15:35 +0300

    Implement open method

--- lib/urfaclient_connection.rb
+++ lib/urfaclient_connection.rb
@@ -1,5 +1,11 @@
+require 'openssl'
+
 class UrfaclientConnection
   attr_accessor :error
+  attr_reader :socket
+
+  CERTIFICATE = 'admin.crt'
+  PASSPHRASE = 'netup'
 
   def initialize(address, port, login, pass, ssl = true, admin = false)
     @admin = admin
@@ -7,7 +13,19 @@ class UrfaclientConnection
     return login_error unless login(login, pass, true)
   end
 
-  def open(*args)
+  def open(address, port)
+    ssl_context = OpenSSL::SSL::SSLContext.new
+    if @admin
+      ssl_context.key = OpenSSL::PKey.read(CERTIFICATE, PASSPHRASE)
+      ssl_context.verify_mode = VERIFY_PEER
+    else
+      ssl_context.ciphers = 'ADH-RC4-MD5'
+    end
+    tcp_socket = TCPSocket.open(address, port)
+    @socket = OpenSSL::SSL::SSLSocket.new(tcp_socket, ssl_context)
+    @socket.sync_close = true
+    @socket.connect
+    @socket.nil?
   end
 
   def login(*args)
