639409aa2393cf04974475df2ce66feafece6750
Koltunov Alexander <firstsano@gmail.com>
2016-11-24 13:55:38 +0300

    Implement Urfaclient class

--- lib/urfaclient.rb
+++ lib/urfaclient.rb
@@ -1,2 +1,17 @@
+require 'urfaclient_connection'
+require 'urfaclient_packet'
+
 class Urfaclient
+  def initialize(login:, password:, admin:, address: "127.0.0.1", port: "11758", ssl: true)
+    @address, @port = address, port
+    @connection = UrfaclientConnection.new(
+      login:    login,
+      password: password,
+      admin:    admin,
+      address:  address,
+      port:     port,
+      ssl:      ssl
+    )
+    @error = @connection.error
+  end
 end
