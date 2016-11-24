03dac061abb018be4610c14c2d7d19e3cfaac396
Koltunov Alexander <firstsano@gmail.com>
2016-11-24 13:56:15 +0300

    Add testing initialization for Urfaclient class

--- spec/lib/urfaclient_spec.rb
+++ spec/lib/urfaclient_spec.rb
@@ -1,5 +1,24 @@
+require 'urfaclient_connection'
 require 'urfaclient'
 
 describe Urfaclient do
+  def urfaclient(params = {})
+    defaults = {
+      login:    "test",
+      password: "test",
+      admin:    false,
+      address:  "127.0.0.1",
+      port:     "11758",
+      ssl:      true
+    }
 
+    Urfaclient.new(**defaults.merge(params))
+  end
+
+  context "on initilization" do
+    it 'should call to creation of UrfaclientConnection instance' do
+      expect(UrfaclientConnection).to receive(:new)
+      urfaclient
+    end
+  end
 end
